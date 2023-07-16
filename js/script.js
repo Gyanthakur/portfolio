82ea747d915ca79c5a73f668293765654a191c48
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-07-16 20:59:53 +0530

    11 commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
-var typed = new Typed(".typing",{
-    strings:["","Web Desiner","Problem Solver","Web Devloper"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
+var typed = new Typed(".typing", {
+	strings: ["", "Web Desiner", "Problem Solver", "Web Devloper"],
+	typeSpeed: 100,
+	BackSpeed: 60,
+	loop: true,
+});
 
-var typed = new Typed(".typing1",{
-    strings:["","Web Desiner","Problem Solver","Web Devloper"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
-
-
+var typed = new Typed(".typing1", {
+	strings: ["", "Web Desiner", "Problem Solver", "Web Devloper"],
+	typeSpeed: 100,
+	BackSpeed: 60,
+	loop: true,
+});
 
 // var hamburger_menu = document.querySelector(".hamburger-menu");
 // var container = document.querySelector(".container1");
